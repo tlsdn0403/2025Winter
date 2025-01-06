ea35e9f5c6a060fce264f1ad656b7460d44a7599
tlsdn0403@NoteBook <tlsdn0403@gmail.com>
2025-01-06 18:36:29 +0900

    설명추가

--- c++Primer/function_235p.cpp
+++ c++Primer/function_235p.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 
 //펙토리얼을 만들어주는 함수
-int fact(int val) {
+int fact(int val) { //fact 함수를 호출 할 때 전달한 변수로 val변수를 초기화
 	int ret = 1;//계산결과를 담는 지역변수
 	while (val > 1) {
 		ret *= val--; //val값을 대입한 후에 1을 뺌 (후위연산자)
@@ -11,7 +11,8 @@ int fact(int val) {
 	return ret;
 }
 int main() {
-	int j = fact(5);
-	cout << "j는 5의 펙토리얼 j의 값은 : " << j;
+	int k = 3.14; //산술변환이 일어남
+	int j = fact(k); //함수를 호출할 때 int 변수 val 생성
+	cout<<k << " 의 펙토리얼 j의 값은 : " << j;
 	return 0;
 }
