12cf6cee64e7c5fe7f946945a5ee87a6b93fa979
tlsdn0403@NoteBook <tlsdn0403@gmail.com>
2025-01-17 17:49:14 +0900

    실습 2-2 소켓 생성과 닫기

--- Server/InitSocket.cpp
+++ Server/InitSocket.cpp
@@ -12,12 +12,70 @@
 
 #pragma comment(lib, "ws2_32") // ws2_32.lib 링크
 
-int main() {
+
+// 소켓 함수 오류 출력 후 종료
+void err_quit(const char* msg)
+{
+	LPVOID lpMsgBuf; //Long Pointer Void void*라고 생각하면 된다
+	FormatMessageA(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, NULL,// 오류메세지를 저장할 공간을 함수가 할당 | 운영체제에서 오류 메세지 가져옴 ,IP소스에는 널
+		WSAGetLastError(), //오류코드를 나타내며, WSAGetLastError()의 리턴값을 넣음 
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), // 오류메세지를 나타낼 언어 한글로 나타냄
+		(char*)&lpMsgBuf, //주솟값을 저장할 변수
+		0, NULL);
+
+	MessageBoxA(NULL, (const char*)lpMsgBuf, msg, MB_ICONERROR);
+	LocalFree(lpMsgBuf); //메모리를 반환해야한다.
+	exit(1);
+}
+
+// 소켓 함수 오류 출력
+void err_display(const char* msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessageA(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(char*)&lpMsgBuf, 0, NULL);
+	printf("[%s] %s\n", msg, (char*)lpMsgBuf);
+	LocalFree(lpMsgBuf);
+}
+
+// 소켓 함수 오류 출력
+void err_display(int errcode)
+{
+	LPVOID lpMsgBuf;
+	FormatMessageA(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, errcode,
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(char*)&lpMsgBuf, 0, NULL);
+	printf("[오류] %s\n", (char*)lpMsgBuf);
+	LocalFree(lpMsgBuf);
+}
+int main(int argc, char* argv[]) 
+{
+	//윈속 초기화
 	WSADATA wsa;
 	if (WSAStartup(MAKEWORD(2, 2), &wsa))
 		return 1;
 	std::cout << "윈속 초기화 성공" << std::endl;
 
+	//소켓 생성
+	SOCKET sock = socket(AF_INET6, SOCK_STREAM, 0);
+	if (sock == INVALID_SOCKET) {
+		int errorCode = WSAGetLastError();
+		std::cout << "소켓 생성 실패, 오류 코드: " << errorCode << std::endl;
+		err_display(errorCode); // 상세 오류 출력
+		err_quit("socket");
+	}
+	std::cout << "소켓 생성 성공" << std::endl;
+
+	//소켓 닫기
+	closesocket(sock);
+
+	//윈속 종료
 	WSACleanup();
 	return 0;
 }
