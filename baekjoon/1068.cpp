35b6b6d0045b3c555dec98f69c7c059e7b2cf6fc
tlsdn0403@NoteBook <tlsdn0403@gmail.com>
2025-02-03 16:57:06 +0900

    자꾸 메모리 초과가 뜸

--- baekjoon/1068.cpp
+++ baekjoon/1068.cpp
@@ -3,37 +3,33 @@
 using namespace std;
 
 int num;
-vector<vector<int>> map(50);
-int findleaf(vector<vector<int>> tree, int fn, int leaf) {
-    int lc;
-    int rc;
-    if (tree[fn].size() <= 1) {  //자식이 없는경우
+vector<vector<int>> map;
+void findleaf(const vector<vector<int>> &tree, int currentNode, int &leaf) {
+    if (tree[currentNode].size() <= 1) {  //자식이 없는경우
         leaf--;
-        return leaf;
+        return;
     }
     else { //자식이 있다면
-        lc = tree[fn][1];
-        leaf = findleaf(tree, lc, leaf);
-        if (tree[fn].size() == 3) {
-            rc = tree[fn][2];
-            leaf = findleaf(tree, rc, leaf);
+        for (int i = 1; i < tree[currentNode].size(); i++) {
+            findleaf(tree, tree[currentNode][i], leaf);
         }
     }
-    return leaf;
+    return;
 }
 
-int answer(vector<vector<int>> tree, int dn) {
+int answer(const vector<vector<int>> &tree, int dn) {
     int leaf = 0;
-    if (dn == 0) {
+    if (dn == 0) { //삭제할 노드가 루트인 경우
         return 0;
     }
-    for (int i = 0; i < num; i++) {
+    for (int i = 0; i < num; i++) { //삭제전 전체 리프노드 수
         if (tree[i].size() <= 1) { //자식이 없다
             leaf++;
         }
     }
-    leaf = findleaf(tree, dn, leaf);
-    if (tree[tree[dn][0]].size() == 2) {
+    findleaf(tree, dn, leaf);
+    int parent = tree[dn][0];
+    if (tree[parent].size() == 2) { //삭제 노드의 부모가 리프노드가 될 경우
         leaf++;
     }
     return leaf;
@@ -42,6 +38,7 @@ int answer(vector<vector<int>> tree, int dn) {
 
 int main() {
     cin >> num;
+    map.resize(num);
     for (int i = 0; i < num; i++) {
         int parent;
         cin >> parent; // 부모 값 입력
