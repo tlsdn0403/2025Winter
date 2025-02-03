e29f6af80a935b013c7079c5ca9667a483aa6d02
tlsdn0403.home_DeskTop <148981969+tlsdn0403@users.noreply.github.com>
2025-02-03 22:20:00 +0900

    정답 제출
    bfs씀

--- baekjoon/1068.cpp
+++ baekjoon/1068.cpp
@@ -14,18 +14,22 @@ bool isInVector(const vector<int>& vec, int deleteNode) {
     }
     return false;
 }
-
+void bfs(const vector<vector<int>>& tree, int cn) {
+    if (tree[cn].empty()) {
+        return;
+    }
+    for (int i : tree[cn]) {
+        deleteParent.push_back(i);
+        bfs(tree, i);
+    }
+}
 int answer(const vector<vector<int>> &tree, int dn) {
     int leaf = 0;
     if (parentNum[dn] == -1) { //삭제할 노드가 루트인 경우
         return 0;
     }
+    bfs(tree, dn);
     for (int i = 0; i < num; i++) { //삭제전 전체 리프노드 수
-        for (int j = 0; j < num; j++) {
-            if (isInVector(deleteParent, parentNum[j])) { //트리의 부모가 deleteParent에 있으면 참 
-                deleteParent.push_back(j);
-            }
-        }
         if (isInVector(deleteParent, parentNum[i])&& !isInVector(deleteParent , i) ) { //순서대로 입력한다는 보장이 없음으로 한 번 더
             deleteParent.push_back(i);
         }
