c1b89b702f82b6e2baf9e1843cef327eab7c7441
tlsdn0403@NoteBook <tlsdn0403@gmail.com>
2025-02-05 17:45:19 +0900

    다각형의 면적
    소수점 1자리로 고정해주고 2.0 으로 나누어 줘야했음

--- baekjoon/2166.cpp
+++ baekjoon/2166.cpp
@@ -2,15 +2,26 @@
 using namespace std;
 
 int n;
-pair<float, float > position[10000];
+pair<long long, long long > position[10000];
+long long CCW(pair<long long, long long> p0, pair<long long, long long> p1, pair<long long, long long> p2) {
+	return (p0.first * p1.second) + (p1.first * p2.second) + (p2.first * p0.second)
+		- (p0.second * p1.first) - (p1.second * p2.first) - (p2.second * p0.first);
+}
 int main()
 {
+	ios_base::sync_with_stdio(false); //c와 c++ 입출력 동기화 해제
+	cin.tie(0); cout.tie(0); //cin과 cout의 묶음을 풀어줌
 	cin >> n;
 	
 	for (int i = 0; i < n; i++) {
 		cin >> position[i].first >> position[i].second;
 	}
-	for (int i = 0; i < n; i++) {
-		cout << position[i].first << ' ' << position[i].second << endl;
+	long long result = 0;
+	for (int i = 1; i < n-1; i++) {
+		result += CCW(position[0], position[i], position[i + 1]);
 	}
+	result /= 2;
+	cout << fixed; //고정시켜줌
+	cout.precision(1); // 소수점 1자리 고정
+	cout << abs(result);
 }
