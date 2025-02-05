b9e96cf6e2089370ab2eaa8657a4545030417216
tlsdn0403@NoteBook <tlsdn0403@gmail.com>
2025-02-05 18:47:56 +0900

    min 함수는 항이 2개여야되나봄

--- baekjoon/1149-RGB거리.cpp
+++ baekjoon/1149-RGB거리.cpp
@@ -4,40 +4,21 @@ using namespace std;
 
 int n;
 int house[1000][3];
-int result = 0;
-void DP(int h[1000][3], int num) {
-	if (num > n) {
-
-		return;
-	}
-	if (num == 0) {
-		++num;
-		DP(h, num);
-		return;
-	}
-	else {
-		h[num][0] += min(h[num - 1][1], h[num - 1][2]);
-		h[num][1] += min(h[num - 1][2], h[num - 1][3]);
-		h[num][2] += min(h[num - 1][0], h[num - 1][1]);
-		++num;
-		result = min(h[num][0], h[num][1], h[num][2]);
-		DP(h, num);
-		return;
-	}
-}
 
 int main()
 {
 	ios_base::sync_with_stdio(false); //c와 c++ 입출력 동기화 해제
 	cin.tie(0); cout.tie(0); //cin과 cout의 묶음을 풀어줌
 	cin >> n;
+	int result = 0;
 	for (int i = 0; i < n; i++) {
-		int red, green, blue;
-		cin >> red >> green >> blue;
-		house[i][0] = red;
-		house[i][1] = green;
-		house[i][2] = blue;
+		cin >> house[i][0] >> house[i][1] >> house[i][2];
 	}
-	DP(house, n);
+	for (int i = 1; i < n; i++) {
+		house[i][0] += min(house[i - 1][1], house[i - 1][2]);
+		house[i][1] += min(house[i - 1][0], house[i - 1][2]);
+		house[i][2] += min(house[i - 1][0], house[i - 1][1]);
+	}
+	result = min({ house[n - 1][0], house[n - 1][1], house[n - 1][2] });
 	cout << result;
 }
