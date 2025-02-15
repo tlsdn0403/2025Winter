3df1a5422962e545b45dbd391532fa172a6ac789
tlsdn0403@NoteBook <tlsdn0403@gmail.com>
2025-02-15 18:06:45 +0900

    7.1.4 연습문제까지

--- c++Primer/Sales_data.h
+++ c++Primer/Sales_data.h
@@ -9,7 +9,8 @@ struct Sales_data {
 	//( )중괄호 사이에 있는 코드는 생성자 초기 값 목록이다.
 	Sales_data(const std:: string&s): bookNo(s){ }
 	Sales_data(const std::string &s, unsigned n ,double p): bookNo(s), units_sold(n), revenue(p*n) { }
-
+	//구조체 생성자의 선언을 나타냄
+	Sales_data(std::istream&); 
 	//매개변수 뒤에 const를 두어 this가 const에 대한 포인터임을 나타냄
 	std::string isbn() const { return this->bookNo; } //클래스는 컴파일러가 맴버선언 후 함수를 처리함으로 bookNo를 사용할 수 있다.
 	Sales_data& combine(const Sales_data&);
@@ -56,5 +57,10 @@ Sales_data add(const Sales_data& lhs, const Sales_data& rhs)
 	Sales_data sum = lhs; //lhs값을 sum에 복사해 넣는다
 	sum.combine(rhs); //rhs의 값을 sum에 더하여 넣는다
 	return sum;
+}
+Sales_data::Sales_data(std::istream& is)
+{
+	std::cout << "입력 스트림 받는 생성자 호출" << std::endl;
+	read(is, *this);
 }
 #endif // !SALES_DATA_H
