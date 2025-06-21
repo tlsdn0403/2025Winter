25cdc68f39a66b06d8551131171430e2ba174c2b
tlsdn0403.home_DeskTop <148981969+tlsdn0403@users.noreply.github.com>
2025-06-22 03:08:26 +0900

    .

---
+++ c++Primer/asdda.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+
+using namespace std;
+
+char board[18][18];
+void resetBoard(char board[18][18]);
+void printBoard(char board[18][18]);
+void imformation(int& x, int& y);
+
+
+int main() {
+
+    resetBoard(board);
+    printBoard(board);
+
+    while (true) {
+        int x = 0, y = 0;
+        imformation(x, y);
+        cout << x << " ," << y << endl;
+    }
+}
+
+
+void resetBoard(char board[18][18])
+{
+    for (int i = 0; i < 18; ++i) {
+        for (int j = 0; j < 18; ++j) {
+            board[i][j] = '+';
+        }
+    }
+}
+void printBoard(char board[18][18])
+{
+    for (int i = 0; i < 18; ++i) {
+        for (int j = 0; j < 18; ++j) {
+            cout << board[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+void imformation(int& x, int& y)
+{
+    cout << "ÁÂÇ¥¸¦ ÀÔ·ÂÇÏ½Ã¿À(x, y)(ÁÂ»ó±Í 0,0/¿ìÇÏ±Í 18,18) : " << endl;
+    cin >> x;
+    cin >> y;
+    while (x > 18 || y > 18) {
+        cout << "Àß¸øµÈ ÀÔ·ÂÀÔ´Ï´Ù. ÁÂÇ¥¸¦ ´Ù½Ã ÀÔ·ÂÇÏ½Ã¿À(x, y)(ÁÂ»ó±Í 0,0/¿ìÇÏ±Í 18,18) : " << endl;
+        cin >> x;
+        cin >> y;
+    }
+}
--- c++Primer/omok_me.cpp
+++ c++Primer/omok_me.cpp
@@ -2,50 +2,65 @@
 
 using namespace std;
 
-char board[18][18];
-void resetBoard(char board[18][18]);
-void printBoard(char board[18][18]);
-void imformation(int& x, int& y);
+char board[19][19];
 
+void reset_board(char board[19][19]);
+void print_board(char board[19][19]);
 
 int main() {
 
-	resetBoard(board);
-	printBoard(board);
-
+	reset_board(board);
+	int turn = 0;
+	int blackCount = 0;
+	int whiteCount = 0;
 	while (true) {
-		int x=0, y = 0;
-		imformation(x, y);
-		cout << x << " ," << y << endl;
+		int x = 0, y = 0;
+		print_board(board);
+		cout << ("좌표를 입력하시오(x, y)(좌상귀 0,0/우하귀 18,18) : ") << '\n';
+		while (true) {
+			cin >> x;
+			cin >> y;
+			if (x < 19 || y < 19) {
+				break;
+			}
+			else {
+				cout << "오목판 안에 두어야 합니다 " << '\n';
+			}
+		}
+		if (board[y][x] == '+') {
+			if (turn % 2 == 0) {
+				board[y][x] = '0';
+				++turn;
+				++blackCount;
+			}
+			else{
+				board[y][x] = '1';
+				++turn;
+				++whiteCount;
+			}
+		}
+		else {
+			cout << "이미 돌을 둔 곳입니다" << endl;
+		}
+		cout << " 흑돌의 개수 :" << blackCount << " , 백돌의 개수 :" << whiteCount << endl;
 	}
 }
 
-
-void resetBoard(char board[18][18])
+void reset_board(char board[19][19])
 {
-	for (int i = 0; i < 18; ++i) {
-		for (int j = 0; j < 18; ++j) {
+	for (int i = 0; i < 19; ++i) {
+		for (int j = 0; j < 19; ++j) {
 			board[i][j] = '+';
 		}
 	}
 }
-void printBoard(char board[18][18])
+
+void print_board(char board[19][19])
 {
-	for (int i = 0; i < 18; ++i) {
-		for (int j = 0; j < 18; ++j) {
+	for (int i = 0; i < 19; ++i) {
+		for (int j = 0; j < 19; ++j) {
 			cout << board[i][j] << " ";
 		}
-		cout << endl;
-	}
-}
-void imformation(int& x, int& y)
-{
-	cout << "좌표를 입력하시오(x, y)(좌상귀 0,0/우하귀 18,18) : " << endl;
-	cin >> x;
-	cin >> y;
-	while (x > 18 || y > 18) {
-		cout << "잘못된 입력입니다. 좌표를 다시 입력하시오(x, y)(좌상귀 0,0/우하귀 18,18) : " << endl;
-		cin >> x;
-		cin >> y;
+		cout << '\n';
 	}
 }
